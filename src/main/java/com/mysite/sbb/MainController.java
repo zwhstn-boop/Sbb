2e53615b10ecc0f0c9bb6192f364f51b1bc19981
zwhstn-boop <zwhstn@gmail.com>
2026-05-07 12:31:19 +0900

    sbb project host testing

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,16 +2,19 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class MainController {
+public class MainController {//http://localhost:8080/
     @GetMapping("/")
-    public void index() {
-        System.out.println("index() 메서드 호출됨!");
+    @ResponseBody
+    public String index() {
+        return "SBB";
     }
 
-    @GetMapping("/hello")
-    public void hello() {
-        System.out.println("hello() 메서드 호출됨!");
+    @GetMapping("/hello") //http://localhost:8080/hello
+    @ResponseBody
+    public String hello() {
+        return "안녕하세요 ^ ^";
     }
 }
